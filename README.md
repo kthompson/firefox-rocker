1fe51350c4f927f9b2a6d33f6378f8523b5c6639
Kevin Thompson <kevin@theautomaters.com>
2018-09-30 19:41:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Rocker gestures for Firefox
+
+Simple back and forward rocker gestures for Firefox
+
+## Goals
+
+* Simple easy to look at and tell there is no privacy concerns since the addon requires access to all pages
+* Back and forward gestures via Left then Right mouse click and Right then Left mouse clicks.
---
+++ rocker.js
@@ -0,0 +1,27 @@
+let requiredMaxDeltaTime = 200; // millis
+var lastClickTime = 0;
+var lastClickButton = -1;
+function getTime() {
+    var d = new Date();
+    return d.getMilliseconds();
+}
+window.addEventListener("mousedown", function (event) {
+    let time = getTime();
+    let deltaMs = time - lastClickTime;
+    let button = event.button;
+    if (deltaMs < requiredMaxDeltaTime) {
+        if (button === 0 && lastClickButton == 2) {
+            // right then left (back)
+            window.history.back();
+            event.preventDefault();
+        }
+        else if (button === 2 && lastClickButton == 1) {
+            // left then right (forward)
+            window.history.forward();
+            event.preventDefault();
+        }
+    }
+    lastClickTime = time;
+    lastClickButton = button;
+});
+//# sourceMappingURL=rocker.js.map
